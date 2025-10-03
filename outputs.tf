8e1dc3d5daf825d01c1c00aa27cd217588f0c4ae
James Hodgkinson <james@terminaloutcomes.com>
2025-10-03 12:07:39 +1000

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output lambda_function_name {
-    value = aws_lambda_function.this.function_name
+output "lambda_function_name" {
+  value = aws_lambda_function.this.function_name
 }
 
-output role_arn {
-    value = aws_iam_role.lambda_role.arn
+output "role_arn" {
+  value = aws_iam_role.lambda_role.arn
 }
 
-output role_name {
-    value = aws_iam_role.lambda_role.name
+output "role_name" {
+  value = aws_iam_role.lambda_role.name
 }
